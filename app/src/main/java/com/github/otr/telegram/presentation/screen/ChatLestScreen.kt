bf7dd68939e4c01e1fb96d70878b0c662ac00fd5
OTR <49165533+OTR@users.noreply.github.com>
2023-03-26 19:28:40 +0600

    [WIP][FEAT] Create a stub for List of Chats

---
+++ app/src/main/java/com/github/otr/telegram/presentation/screen/ChatLestScreen.kt
@@ -0,0 +1,58 @@
+package com.github.otr.telegram.presentation.screen
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.github.otr.telegram.domain.entity.Chat
+
+@Preview
+@Composable
+fun ChatListScreen(
+    chatRoster: List<Chat> = listOf(
+        Chat(1L, "Andrew"),
+        Chat(2L, "Bob"),
+        Chat(3L, "Charly"),
+        Chat(4L, "Danny"),
+    )
+) {
+    Surface(
+        color = MaterialTheme.colors.background,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize()
+        ) {
+            items(items = chatRoster, key = { it.id }) { chatItem ->
+                Card(
+                    elevation = 8.dp,
+                    backgroundColor = MaterialTheme.colors.background,
+                    border = BorderStroke(
+                        width = 1.dp,
+                        color = MaterialTheme.colors.onBackground
+                    ),
+                    modifier = Modifier.fillMaxSize()
+                )
+                {
+                    Text(text = "Chat with ${chatItem.name}")
+                }
+            }
+
+        }
+    }
+}
+
