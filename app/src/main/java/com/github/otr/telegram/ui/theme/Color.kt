fd1cb224eacc092dc5a9b1da630472cd3c870df3
OTR <49165533+OTR@users.noreply.github.com>
2023-03-29 11:05:25 +0600

    [CHORE] Add Colors and Font Styles to the application's theme

--- app/src/main/java/com/github/otr/telegram/ui/theme/Color.kt
+++ app/src/main/java/com/github/otr/telegram/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package com.github.otr.telegram.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Purple100 = Color(0xFFE1BEE7)
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
--- app/src/main/java/com/github/otr/telegram/ui/theme/Type.kt
+++ app/src/main/java/com/github/otr/telegram/ui/theme/Type.kt
@@ -3,6 +3,7 @@ package com.github.otr.telegram.ui.theme
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
@@ -12,17 +13,18 @@ val Typography = Typography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
+    ),
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.W500,
         fontSize = 14.sp
-    ),
+    )*/
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily.Monospace,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontStyle = FontStyle.Italic,
+        fontSize = 12.sp,
     )
-    */
+
 )
