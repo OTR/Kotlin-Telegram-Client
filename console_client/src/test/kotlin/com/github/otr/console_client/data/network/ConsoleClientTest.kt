797383c8fc8acbe10a5f02209ed15729f95ec151
OTR <49165533+OTR@users.noreply.github.com>
2023-05-14 13:27:28 +0600

    [CHORE] Add dependency for JUnit 4

---
+++ console_client/src/test/kotlin/com/github/otr/console_client/data/network/ConsoleClientTest.kt
@@ -0,0 +1,22 @@
+package com.github.otr.console_client.data.network
+
+import org.junit.Assert.*
+
+class ConsoleClientTest {
+
+    @org.junit.Test
+    fun getStateFlow() {
+    }
+
+    @org.junit.Test
+    fun emitState() {
+    }
+
+    @org.junit.Test
+    fun addHandlers() {
+    }
+
+    @org.junit.Test
+    fun main() {
+    }
+}
