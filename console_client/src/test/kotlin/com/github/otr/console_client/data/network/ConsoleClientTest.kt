66dfd0dc48621aeaf89806f55dc5b61f6a7b44ce
OTR <49165533+OTR@users.noreply.github.com>
2023-05-14 15:56:40 +0600

    [TEST] Create a test that check the sequence of received Auth States

--- console_client/src/test/kotlin/com/github/otr/console_client/data/network/ConsoleClientTest.kt
+++ console_client/src/test/kotlin/com/github/otr/console_client/data/network/ConsoleClientTest.kt
@@ -1,6 +1,14 @@
 package com.github.otr.console_client.data.network
 
+import com.github.otr.console_client.data.network.handler.HandlerType
 import com.github.otr.console_client.domain.entity.AuthState
+import jdk.jfr.Description
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 import org.junit.Assert.assertEquals
 import org.junit.Rule
@@ -17,7 +25,7 @@ class ConsoleClientTest {
     val tempFolder: TemporaryFolder = TemporaryFolder()
 
     @Test
-    fun testCreateBrandNewClient() {
+    fun testCreateInstanceOfConsoleClient() {
         // GIVEN
         // For Windows Temporary Folder would be like:
         // C:\Users\User\AppData\Local\Temp\junit12895079684515193141
@@ -34,5 +42,53 @@ class ConsoleClientTest {
 
         assertEquals(expectedState, actualState)
     }
+
+    @Test
+    fun testAddHandlerToConsoleClient() {
+        // GIVEN
+        val resourcesPath: String = tempFolder.root.path
+        val consoleCLI: ConsoleClient = ConsoleClient(resourcesPath)
+
+        // WHEN
+        consoleCLI.addHandlers(HandlerType.COMMON)
+
+        // THEN
+        val actualState: AuthState = consoleCLI.stateFlow.value
+        val expectedState: AuthState = AuthState.INITIAL
+
+        assertEquals(expectedState, actualState)
+    }
+
+    @Test
+    fun testMainMethodOfConsoleClient() {
+        // GIVEN
+        val resourcesPath: String = tempFolder.root.path
+        val consoleCLI: ConsoleClient = ConsoleClient(resourcesPath)
+        consoleCLI.addHandlers(HandlerType.COMMON)
+        val stateSequence: MutableList<AuthState> = mutableListOf(
+            AuthState.INITIAL,
+            AuthState.WAIT_TD_LIB_PARAMETERS,
+            AuthState.WAIT_PHONE_NUMBER
+        )
+        stateSequence.take(1)
+
+        // WHEN
+        val scope = CoroutineScope(Dispatchers.Default)
+        // THEN
+        scope.launch {
+            consoleCLI.stateFlow.collect {
+                val expectedState: AuthState = stateSequence.removeAt(0)
+                val actualState: AuthState = it
+                println(it)
+                assertEquals(expectedState, actualState)
+            }
+        }
+        scope.launch { consoleCLI.main() }
+        Thread.sleep(5000)
+    }
+
+    // TODO: Test emitting States
+//    consoleCLI.emitState(AuthState.LOGGING_OUT)
+//    Thread.sleep(500)
 
 }
